c36f407fe03338fa8726b90c8de12dffcd2a246a
George Michas <geomixas@hotmail.com>
2016-09-22 12:18:22 +0300

    Create mod.h

---
+++ mod.h
@@ -0,0 +1,9 @@
+#ifndef MOD
+#define MOD
+#include <stdio.h>
+
+unsigned long long int pr(unsigned long long int,unsigned long long int,unsigned long long int);
+unsigned long long int recursion(unsigned long long int,unsigned long long int);
+
+int solution();
+#endif
